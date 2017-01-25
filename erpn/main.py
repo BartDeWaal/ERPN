14b7a65f50b0cb169fdaaf52f6c1694a18208ca7
Bart de Waal <bart@abnormail.org>
2017-01-25 16:35:48 +0100

    Move button mappings into it's own file

--- erpn/main.py
+++ erpn/main.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 
 from . import functions
 from . import cursesHelper
+from .buttonMappings import loadMappings
 from .domain import Reals
 
 stack = []  # The stack as displayed to the unit
@@ -216,56 +217,10 @@ class Interface:
         cursesHelper.displayFromBottom(self.stackWindow, lines)
 
 
+
+
 interface = Interface()
-# See utils folder for script that helps figure out what each key does
-interface.add('x', functions.Delete())
-interface.add('z', functions.switch2)
-interface.add('+', functions.addition)
-interface.add('a', functions.addition)
-interface.add('s', functions.subtract)
-interface.add('-', functions.subtract)
-interface.add('m', functions.multiply)
-interface.add('*', functions.multiply)
-interface.add('d', functions.divide)
-interface.add('/', functions.divide)
-interface.add('p', functions.exponent)
-interface.add('q', functions.square)
-interface.add('S', functions.sqrt)
-interface.add('E', functions.power_e)
-interface.add('e', functions.power_10)  # Should be bound to e for compatability with "1e3" notation
-interface.add('L', functions.log10)
-interface.add('l', functions.ln)
-interface.add('I', functions.mult_inverse)
-interface.add('i', functions.add_inverse)
-interface.add('M', functions.modulo)
-interface.add('%', functions.modulo)
-interface.add('#', functions.gcd)
-interface.add('!', functions.factorial)
-interface.add('`', functions.floor)
-interface.add('~', functions.ceil)
-interface.add('!e', functions.e)
-interface.add('!p', functions.pi)
-
-interface.add('!t', functions.tan)
-interface.add('!s', functions.sin)
-interface.add('!c', functions.cos)
-interface.add('!T', functions.arctan)
-interface.add('!S', functions.arcsin)
-interface.add('!C', functions.arccos)
-
-interface.add('t', functions.copy_from_stack)
-interface.add(' ', functions.CopyCurrent(display=False))
-interface.add('^J', functions.CopyCurrent(display=False))
-interface.add('u', functions.undo)
-interface.add('Q', functions.quit)
-interface.add('c', functions.copy_to_OS)
-interface.add('v', functions.paste_from_OS)
-
-interface.add('^C', functions.arrow_up)
-interface.add('k', functions.arrow_up)
-interface.add('^B', functions.arrow_down)
-interface.add('j', functions.arrow_down)
-
+loadMappings(interface)
 
 def cursesMain(screen):
     """ Main entrypoint, sets up screens etc. """
