c2a2aebd58161d7855685ec04ccb3e49fc95fc87
Bart de Waal <bart@abnormail.org>
2017-01-29 21:29:22 +0100

    Add utility to check what urwid calls keys

---
+++ erpn/utils/urwidKeyName.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Util to find out what urwid calls key
+# Adapted from example in the tutorial
+
+import urwid
+
+def show_or_exit(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+    txt.set_text(repr(key))
+
+palette = [('quoteColor', 'light red', 'default'),]
+txt = urwid.Text("Press key to display name, q to quit")
+fill = urwid.Filler(txt, 'top')
+loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
+loop.run()
