82c290e7a1cd31d34a4f24a2fb37835f5c56c3cb
Bart de Waal <bart@abnormail.org>
2017-02-09 23:48:57 +0100

    Add --version option to program

--- erpn/main.py
+++ erpn/main.py
@@ -4,15 +4,32 @@
 # This program is licenced under the GPL version 3, see Licence file for details
 
 import urwid
+from argparse import ArgumentParser
 
 from .buttonMappings import loadMappings
 from .urwidInterface import Interface
+
+version = '0.3'
+website = 'https://github.com/BartDeWaal/ERPN'
 
 interface = Interface()
 loadMappings(interface)
 
 
 def main():
+    parser = ArgumentParser(
+        prog="erpn",
+        description='An RPN calculator',
+        epilog='See website for full instructions: {}'.format(website))
+    parser.add_argument('--version', dest='version',
+                        action='store_const', const=True,
+                        help='show the version number and exit')
+    args = parser.parse_args()
+    if args.version is True:
+        print("erpn {}\n{}".format(version, website))
+        return
+
+
     palette = [('arrow', 'yellow', 'default'),
                ('lineLabel', 'dark cyan', 'default'),
                ('error', 'light red', 'default')]
